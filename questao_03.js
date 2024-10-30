836c60b268373aaa9440181a49dcecb62bc4f432
RafaelRocha11 <161595793+RafaelRocha11@users.noreply.github.com>
2024-10-30 16:10:45 -0300

    Update questao_03.js

--- questao_03.js
+++ questao_03.js
@@ -4,7 +4,7 @@ let nota2 = 9;
 
 let multNota1 = nota1 * 0.6;
 let multNota2 = nota2 * 0.4;
-let somaNotas = multNota1 + multNota2
+let somaNotas = multNota1 + multNota2;
 
 alert("Nome do aluno: " + nome
       "Nota 1: " + nota1
