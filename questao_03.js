3ae2a1f6709a3d5e41e5e65c7f6bb6ce7fc74b35
RafaelRocha11 <161595793+RafaelRocha11@users.noreply.github.com>
2024-10-30 09:37:32 -0300

    quest3

---
+++ questao_03.js
@@ -0,0 +1,12 @@
+let nome = "Rafael Rocha";
+let nota1 = 8;
+let nota2 = 9;
+
+let multNota1 = nota1 * 0.6;
+let multNota2 = nota2 * 0.4;
+let somaNotas = multNota1 + multNota2
+
+alert("Nome do aluno: " + nome)
+alert("Nota 1: " + nota1)
+alert("Nota 2: " + nota2)
+alert("Nota total: " + somaNotas)
