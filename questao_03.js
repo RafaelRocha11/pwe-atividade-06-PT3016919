59c3f43f03ce7995f5b0ab4025785076682c6ed7
RafaelRocha11 <161595793+RafaelRocha11@users.noreply.github.com>
2024-10-30 15:29:02 -0300

    Update questao_03.js

--- questao_03.js
+++ questao_03.js
@@ -6,7 +6,8 @@ let multNota1 = nota1 * 0.6;
 let multNota2 = nota2 * 0.4;
 let somaNotas = multNota1 + multNota2
 
-alert("Nome do aluno: " + nome)
-alert("Nota 1: " + nota1)
-alert("Nota 2: " + nota2)
-alert("Nota total: " + somaNotas)
+alert("Nome do aluno: " + nome
+      "Nota 1: " + nota1
+      "Nota 2: " + nota2)
+alert("Nome do aluno: " + nome
+      "Nota total: " + somaNotas)
