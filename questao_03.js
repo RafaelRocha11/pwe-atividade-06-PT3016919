889479f9e7d55073c6c07037a334e71b21cce531
RafaelRocha11 <161595793+RafaelRocha11@users.noreply.github.com>
2024-10-31 22:10:30 -0300

    quest3

--- questao_03.js
+++ questao_03.js
@@ -6,7 +6,5 @@ let multNota1 = nota1 * 0.6;
 let multNota2 = nota2 * 0.4;
 let somaNotas = multNota1 + multNota2;
 
-alert("Nome do aluno: " + nome);
-alert("Nota 1: " + nota1);
-alert("Nota 2: " + nota2);
-alert("Nota final: " + somaNotas);
+alert("Nome do aluno: " + nome + "\nNota 1: " + nota1 + "\nNota 2: " + nota2);
+alert("Nome do aluno: " + nome + "\nNota final: " + somaNotas);
